8db3ced2ac91342b33142dab6d35dc9be1e110c8
Venodez <Venodez@users.noreply.github.com>
2020-02-15 06:44:06 +0100

    Ajout de l'event quand un joueur quitte le serveur

--- Astiria.js
+++ Astiria.js
@@ -34,9 +34,16 @@ bot.on("ready", () => {
 })
 
 bot.on("guildMemberAdd", (member) => {
+	
 	let joueur = member.guild.members.filter(member => !member.user.bot).size;
 	member.guild.channels.get(config.channel).send(`>>> <:Astiria:676218452125679661> 》**ASTIRIA | BIENVENUE**\nBienvenue à ${member} sur Astiria !\n● Le Discord compte désormais ${joueur} personnes`);
 
 })
+
+bot.on("guildMemberRemove", (member) => {
+
+	let joueur = member.guild.members.filter(member => !member.user.bot).size;
+	member.guild.channels.get(config.channel).send(`>>> <:Astiria:676218452125679661> 》**ASTIRIA | AUREVOIR**\nAurevoir à ${member} au plaisir de le revoir !\n● Le Discord compte désormais ${joueur} personnes`);
+}
 
 bot.login(config.token);
