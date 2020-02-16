3a178a168451dd26102fe1267e027d286a1f6313
Venodez <Venodez@users.noreply.github.com>
2020-02-16 04:14:14 +0100

    Oublie d'une "(" dans le code

--- Astiria.js
+++ Astiria.js
@@ -44,6 +44,6 @@ bot.on("guildMemberRemove", (member) => {
 
 	let joueur = member.guild.members.filter(member => !member.user.bot).size;
 	member.guild.channels.get(config.channel).send(`>>> <:Astiria:676218452125679661> 》**ASTIRIA | AUREVOIR**\nAurevoir à ${member} au plaisir de le revoir !\n● Le Discord compte désormais ${joueur} personnes`);
-}
+})
 
 bot.login(config.token);
